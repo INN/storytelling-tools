8d08ecd48676a4ea348c4d52f66c34da91b2df62
rclations <contact@rclations.com>
2017-01-24 15:50:58 -0500

    cleanup

--- knight-lab-storytelling-tools.php
+++ knight-lab-storytelling-tools.php
@@ -35,11 +35,6 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-/**
- * Built using generator-plugin-wp
- */
-
-
 // Include additional php files here.
  require 'includes/class-settings.php';
 
