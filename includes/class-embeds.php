01915b6a0db5dcca5e071cf4f54b610a4a12b3ce
rclations <contact@rclations.com>
2017-01-24 17:09:27 -0500

    added timeline js embed handler

--- includes/class-embeds.php
+++ includes/class-embeds.php
@@ -39,5 +39,23 @@ class KLST_Embeds {
 	 * @return void
 	 */
 	public function hooks() {
+		wp_embed_register_handler(
+			'knight-lab-timeline',
+			'#https://cdn\.knightlab\.com/libs/timeline3/latest/embed/index\.html\?source=([a-zA-Z0-9_-]+)&font=([a-zA-Z0-9_-]+)&lang=([a-zA-Z0-9-]+)&initial_zoom=([\d]+)&height=([\d]+)#i',
+			array( $this, 'wp_embed_knight_lab_timeline' )
+		);
+	}
+
+	public function wp_embed_knight_lab_timeline( $matches, $attr, $url, $rawattr ) {
+		$embed = sprintf(
+			'<iframe src="https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=%1$s&font=%2$s&lang=%3$s&initial_zoom=%4$s&height=%5$s" width="100%%" height="650" webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder="0"></iframe>',
+			esc_attr( $matches[1] ),
+			esc_attr( $matches[2] ),
+			esc_attr( $matches[3] ),
+			esc_attr( $matches[4] ),
+			esc_attr( $matches[5] )
+		);
+
+		return apply_filters( 'embed_forbes', $embed, $matches, $attr, $url, $rawattr );
 	}
 }
