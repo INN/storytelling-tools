b292bf12fce218f8795266fa3a86714a20de5ef7
rclations <contact@rclations.com>
2017-07-08 09:47:08 -0400

    removing cmb2

--- includes/class-settings.php
+++ includes/class-settings.php
@@ -111,7 +111,6 @@ class KLST_Settings {
 		?>
 		<div class="wrap cmb2-options-page <?php echo esc_attr( $this->key ); ?>">
 			<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
-			<?php cmb2_metabox_form( $this->metabox_id, $this->key ); ?>
 		</div>
 		<?php
 	}
