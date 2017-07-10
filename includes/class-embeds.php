ec8e1e4c84500f028a4285f14c295d19b2648176
rclations <contact@rclations.com>
2017-07-10 15:54:31 -0400

    improved matching for timeline embeds

--- includes/class-embeds.php
+++ includes/class-embeds.php
@@ -41,7 +41,7 @@ class KLST_Embeds {
 	public function hooks() {
 		wp_embed_register_handler(
 			'knight-lab-timeline',
-			'#https://cdn\.knightlab\.com/libs/timeline3/latest/embed/index\.html\?source=([a-zA-Z0-9_-]+)&font=([a-zA-Z0-9_-]+)&lang=([a-zA-Z0-9-]+)&initial_zoom=([\d]+)&height=([\d]+)#i',
+			'#https://cdn\.knightlab\.com/libs/timeline/latest/embed/\?source=[a-zA-Z0-9_-]+#i',
 			array( $this, 'wp_embed_knight_lab_timeline' )
 		);
 		wp_embed_register_handler(
@@ -67,13 +67,23 @@ class KLST_Embeds {
 	 * @since  1.0.0
 	 */
 	public function wp_embed_knight_lab_timeline( $matches, $attr, $url, $rawattr ) {
-		$embed = sprintf(
+		$parsed_url = wp_parse_url( $url );
+		parse_str( $parsed_url['query'], $args );
+
+		$defaults = array(
+			'source' => '',
+			'font' => '',
+			'lang' => '',
+			'initial_zoom' => '4',
+			'height' => '650',
+			'width' => '100%%'
+		);
+
+		$args = wp_parse_args( $args, $defaults );
+
+		$embed = vsprintf(
 			'<iframe src="https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=%1$s&font=%2$s&lang=%3$s&initial_zoom=%4$s&height=%5$s" width="100%%" height="650" webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder="0"></iframe>',
-			esc_attr( $matches[1] ),
-			esc_attr( $matches[2] ),
-			esc_attr( $matches[3] ),
-			esc_attr( $matches[4] ),
-			esc_attr( $matches[5] )
+			$args
 		);
 		return apply_filters( 'embed_knight_lab_timeline', $embed, $matches, $attr, $url, $rawattr );
 	}
